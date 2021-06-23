475e0926a534d4d098211f03a6bdef61278d4904
Troy Johnson <tnj8510@gmail.com>
2021-06-22 23:53:31 -0700

    final changes for sumbission

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,13 +1,14 @@
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
+// get the button that opens the modal
 var btn = document.getElementById("myBtn");
+
 var btnNav = document.getElementById("myBtnNav"); //For nav bar
 
-// Get the <span> element that closes the modal
+// get the <span> element that closes the modal
 var modalClose = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
+// when the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
 }
@@ -16,12 +17,12 @@ btnNav.onclick = function() { //For nav Bar
   modal.style.display ="block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// when the user clicks on <span> (x), close the modal
 modalClose.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// when the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
